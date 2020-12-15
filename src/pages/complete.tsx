aeb1977864c9503212599973ec5972f5c704990a
Tyler Newman <tylerjnewman18@gmail.com>
2020-12-15 13:15:20 -0800

    Change color of complete page

--- src/pages/complete.tsx
+++ src/pages/complete.tsx
@@ -4,7 +4,12 @@ import Header from 'src/components/sections/Header'
 import CompleteView from 'src/components/sections/CompleteView'
 
 const Complete = () => (
-  <Layout height="100vh" overflow="scroll">
+  <Layout
+    height="100vh"
+    overflow="scroll"
+    lightColor={'orange.100'}
+    darkColor={'purple.500'}
+  >
     <Header />
     <CompleteView />
   </Layout>
