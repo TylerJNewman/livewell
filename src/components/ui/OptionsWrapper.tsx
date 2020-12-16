59cfa473f3ccb8e6435b84ce820f36cb7dbeec37
Tyler Newman <tylerjnewman18@gmail.com>
2020-12-15 17:19:40 -0800

    Try again

--- src/components/ui/OptionsWrapper.tsx
+++ src/components/ui/OptionsWrapper.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const OptionsWrapper = (props: FlexProps) => {
   return (
-    <Box pt={['10px', '80px', '116px']} pb="97px" px="10px">
+    <Box pt={['80px', '116px']} pb="97px" px="10px">
       <Flex flexBasis="auto" flexGrow={1} w="100%" h="100%">
         {props.children}
       </Flex>
